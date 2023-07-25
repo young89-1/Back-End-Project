59dff7317d0e9372e0f438613c417cf6f6da71e8
young89-1 <mny76ers@yahoo.com>
2023-07-25 16:55:45 -0400

    fixed main

--- routes/podcastService.js
+++ routes/podcastService.js
@@ -1,29 +1,43 @@
-//const { Client } = require('podcast-api');
+const { Client } = require('podcast-api');
+require("dotenv").config()
+const api = process.env.APIKEY
 
 // Create a function to fetch podcast genres
 const fetchPodcastGenres = async () => {
-  const client = Client({ apiKey: 'bac9507b968e4f6bb25327dd6a9765fd' });
+  const client = Client({ apiKey: api });
 
   try {
     const response = await client.fetchPodcastGenres({ top_level_only: 1 });
-    return response.data; // Return the genre data
+    return response.data.genres; // Return the genre data
   } catch (error) {
     console.error('Error fetching podcast genres:', error);
     throw error; // Rethrow the error to be handled by the caller
   }
 };
 
-const client = Client({ apiKey: 'bac9507b968e4f6bb25327dd6a9765fd' });
-client.fetchPodcastById({
-  id: '4d3fe717742d4963a85562e9f84d8c79',
-  next_episode_pub_date: 1479154463000,
-  sort: 'recent_first',
-}).then((response) => {
-  // Get response json data here
-  console.log(response.data);
-}).catch((error) => {
-  console.log(error)
-});
+const fetchPodcast = async (title, genre) => {
+  try {
+    const client = Client({ apiKey: api });
+    const response = await client.search({
+      q: title,
+      genre_ids: genre,
+      language: 'English',
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error('Error fetching podcasts:', error);
+    throw error; // Rethrow the error to be handled by the caller
+  }
+};
 
-module.exports = { fetchPodcastGenres, 
-  fetchPodcastById };
+module.exports = { fetchPodcastGenres, fetchPodcast };
+
+
+
+
+
+
+
+
+
+

2da15344b9d4267bbaeb7d3b4f10886916c6cd79
young89-1 <mny76ers@yahoo.com>
2023-08-22 21:20:23 -0400

    added js

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+
+module.exports =
+{
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": process.env.DB_TEST_USER,
+    "password": process.env.DB_TEST_PASS,
+    "database": process.env.DB_TEST_DATA,
+    "host": process.env.DB_TESTPRO_HOST,
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.DB_TEST_USER,
+    "password": process.env.DB_TEST_PASS,
+    "database": process.env.DB_PROD_DATA,
+    "host": process.env.DB_TESTPRO_HOST,
+    "dialect": "postgres"
+  }
+}

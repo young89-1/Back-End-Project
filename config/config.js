ff37d8106794b94a4e67bbbc809986c7542f4295
young89-1 <mny76ers@yahoo.com>
2023-08-25 17:49:56 -0400

    fixed production

--- config/config.js
+++ config/config.js
@@ -17,10 +17,10 @@ module.exports =
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_TEST_USER,
-    "password": process.env.DB_TEST_PASS,
-    "database": process.env.DB_PROD_DATA,
-    "host": process.env.DB_TESTPRO_HOST,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   }
 }
